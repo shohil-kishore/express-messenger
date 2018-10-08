3c61fa3f68a3a09b3e5361d94c9f68c47944def1
shohil-kishore <s.kishore@auckland.ac.nz>
2018-10-08 14:42:00 +1300

    Added comments

--- client/client.js
+++ client/client.js
@@ -1,12 +1,15 @@
 const form = document.querySelector("form");
 const loadingElement = document.querySelector(".loading");
 const messagesElement = document.querySelector(".messages");
-const API_URL = window.location.hostname === 'localhost' ? "http://localhost:5000/messages" : 'https://express-messenger-api.now.sh/messages';
+const API_URL = window.location.hostname === 'localhost' ? "http://localhost:5000/messages" : 'https://express-messenger-api.now.sh/messages'; // If not localhost, use API.
 
-loadingElement.style.display = "none";
+loadingElement.style.display = "none"; // Sets loading GIF display.
 
+// Fetch all messages from DB.
 listAllMessages()
 
+// On submit, store data and display loading icon. 
+// After POST request, disable form for five seconds (client-side). Then, using listAllMessages, perform a GET request.
 form.addEventListener("submit", event => {
   event.preventDefault();
   const formData = new FormData(form);
@@ -39,6 +42,7 @@ form.addEventListener("submit", event => {
     });
 });
 
+// Clears innerHTML and adds a new div for each element in the DB. 
 function listAllMessages() {
   messagesElement.innerHTML = "";
   fetch(API_URL)
@@ -46,15 +50,13 @@ function listAllMessages() {
     .then(messages => {
       console.log(messages);
       messages.reverse()
+      
       messages.forEach(message => {
         const div = document.createElement('div');
-
         const header = document.createElement('h3');
         header.textContent = message.name;
-
         const contents = document.createElement('p');
         contents.textContent = message.content;
-
         const date = document.createElement('small');
         date.textContent = new Date(message.created);
 
--- server/app.js
+++ server/app.js
@@ -5,25 +5,28 @@ const Filter = require("bad-words");
 const rateLimit = require("express-rate-limit");
 
 const app = express();
-const db = monk(process.env.MONGO_URI || "localhost/express_messenger");
-const messages = db.get("messages");
-filter = new Filter();
+const db = monk(process.env.MONGO_URI || "localhost/express_messenger"); // Points to mLab.
+const messages = db.get("messages"); 
+filter = new Filter(); // Bad words filter.
 
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
   res.send({
-    message: "Hello!"
+    message: "Welcome to the express-mess API! Please goto /messages to see all messages."
   });
 });
 
+// Find all messages in DB.
 app.get("/messages", (req, res) => {
   messages.find().then(messages => {
     res.json(messages);
   });
 });
 
+// Basic blank input checker (use middleware instead).
 function isValidMessage(message) {
   return (
     message.name &&
@@ -33,6 +36,7 @@ function isValidMessage(message) {
   );
 }
 
+// Limits POST request, but not GET (that's why it's placed AFTER the GET request).
 app.use(
   rateLimit({
     windowMs: 10 * 1000,
@@ -40,6 +44,7 @@ app.use(
   })
 );
 
+// On POST, if message is not blank, input is cleaned and pushed to DB. 
 app.post("/messages", (req, res) => {
   if (isValidMessage(req.body)) {
     const message = {
@@ -60,6 +65,7 @@ app.post("/messages", (req, res) => {
   console.log(req.body);
 });
 
+// App listening on port 5000.
 app.listen(5000, () => {
   console.log("Listening on http://localhost:5000");
 });
